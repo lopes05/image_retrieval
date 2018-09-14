2e2c1e18a68e457c7a1d03e5d022e4455e2eaff9
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2018-09-14 17:22:49 -0300

    Create histograms of all images and save into file

--- image.py
+++ image.py
@@ -2,6 +2,8 @@ import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import ast
+import os
 
 def to_grayscale(img):
     # (32 ,32, 1)
@@ -17,11 +19,59 @@ def calc_histograma(img):
     
     return dic
 
+def save_histograms(histogramas):
+    with open('histograms.txt', 'a') as f:
+        for i in histogramas.keys():
+            w = str(i) + '#' + str(histogramas[i]) + '\n'
+            f.write(w)
+        f.close()
+
+def build_all_histograms(path):
+    histogramas = {}
+    cont = 1
+    print('Init histograms')
+    for fil in os.listdir(path):
+        print(fil)
+        img = to_grayscale(cv2.imread(f'{path}/{fil}'))
+        hist = calc_histograma(img)
+        histogramas[fil] = hist
+        cont += 1
+
+    save_histograms(histogramas)
+    print('Histograms saved!')
+    return histogramas
+
+import math
+
+def euclidian_distance(v1, v2):
+    return (v1 - v2)**2
+
+def rank_images(img_hist, histograms):
+    files = list(histograms.keys())
+    euclidian_diff = {}
+    for f in files:
+        euclidian_diff[f] = euclidian_diff.get(f, 0)
+        for pix_value in img_hist:
+            pix_distance = euclidian_distance(img_hist[pix_value], histograms[f][pix_value])
+            euclidian_diff[f] += pix_distance
+        euclidian_diff[f] = math.sqrt(euclidian_diff[f])
+    return euclidian_diff
+
 if __name__ == "__main__":
-    img = cv2.imread('imgdatabase/wrecker_s_001348.png')
+    img = cv2.imread('corel1000/Africa81.jpg')
     aux = to_grayscale(img)
-    #cv2.imshow('img', aux)
-    #cv2.waitKey(0)
-    #cv2.imwrite('batata.png', aux)
+    
     histogram = calc_histograma(aux)
-    print(histogram)
+    
+    if os.path.exists('./histograms.txt'):
+        hists = {}
+
+        with open('histograms.txt', 'r') as f:
+            lines = [line.split('#') for line in f]
+            for line in lines:
+                hists[line[0]] = hists.get(line[0], {})
+                hists[line[0]] = ast.literal_eval(line[1])
+            
+    else:
+        hists = build_all_histograms('corel1000')
+    print(rank_images(histogram, hists))

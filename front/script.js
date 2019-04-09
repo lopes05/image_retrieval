5fd71d9a89ccbe43c759892773103667d9829a51
Gustavo <gustavo.ldbrito@gmail.com>
2019-04-09 18:13:35 -0300

    front upd

--- front/script.js
+++ front/script.js
@@ -1,5 +1,4 @@
 window.$ = window.jQuery = require('jquery'); // not sure if you need this at all
-window.Bootstrap = require('bootstrap');
 
 
 var dados;
@@ -8,14 +7,29 @@ function refilter()
 {   
 
     imgs = [];
+    var sel = document.getElementById('selectbox');
+    var tec = document.getElementById('tecnica');
     for (d in dados){
-        el = document.getElementById(d)
-        var pacote = {'img': d, 'relevant': el.checked};
+        el = document.getElementById(d + "d");
+        el2 = document.getElementById(d + "i");
+
+        if(d.includes(sel.value)){
+            el.checked = true;
+            el2.checked = false;
+        }
+        else{
+            el.checked = false;
+            el2.checked = true;
+        }
+
+        var pacote = {'img': d, 'relevant': el.checked, 'irrelevant': el2.checked };
         imgs.push(pacote);
     }
+    imgs.push({'classname': sel.value});
+    imgs.push({'tecnica': tec.value});
     json = JSON.stringify(imgs);
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://localhost:5000/refilter', true); // or https://example.com/upload/image
+    xhr.open("POST", 'http://localhost:5000/refilter', true);
     xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
     xhr.onreadystatechange = function(e) {
         if (xhr.readyState === 4) {
@@ -36,13 +50,12 @@ function recreate()
     while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
     }
-}
+};
 
 
 function upload_file() 
 {
     let photo = document.getElementById("imgurl").files[0] // simlar to: document.getElementById("image-file").files[0] 
-   
 
     let formData = new FormData();
     formData.append("imgurl", photo);  
@@ -53,6 +66,9 @@ function upload_file()
           if (xhr.status === 200) {
             console.log(xhr.responseType)
             loadgallery(xhr);
+            var element = document.getElementById("footer");
+            element.classList.remove("position-fixed");
+            element.classList.add("position-relative");
           } 
         }
     }
@@ -62,8 +78,9 @@ function upload_file()
 };
 
 
-function loadgallery(json_data, first) {
 
+function loadgallery(json_data, first) {
+    $('#imagem').hide();
     var arr = JSON.parse(json_data.response);
     
     str = "";
@@ -71,9 +88,10 @@ function loadgallery(json_data, first) {
     for (ke in arr){
         
         str = str + '<div class="col-lg-3 col-md-4 col-xs-6">'
-            + '<input id="' + ke + '" name="dados" type="checkbox"\> Relevante?'
+            + '<input id="' + ke + 'd" name="dados" type="checkbox"\> Relevante?'
+            + '<input id="' + ke + 'i" name="dados" type="checkbox"\> Irrelevante?'
             + '<a href="#" class="d-block mb-4 h-100">' + 
-            '<img class="img-fluid img-thumbnail" src="/home/gustavo/GustavoUNB/tcc/corel1000/' + ke + '" alt="' + arr[ke] + '"\>'
+            '<img class="img-fluid img-thumbnail img-responsive" style="width:300px;height: 150px;" src="/home/gustavo/GustavoUNB/tcc/corel1000/' + ke + '" alt="' + arr[ke] + '"\>'
             + "<\a> </div>";
     }
     
@@ -84,5 +102,6 @@ function loadgallery(json_data, first) {
     
     document.getElementById("formbusca").style.display="none";
     document.getElementById("imagens").style.display="block";
+    document.getElementById("refilter").style.display="block";
     document.getElementById("realimagens").append(k);
 };
